f33726fed6630357bc09df083af15669d95a804f
Hania <hannacicha10@gmail.com>
2025-12-23 18:39:17 +0100

    Uzupełnienie braków w module logic.py

---
+++ logic.py
@@ -0,0 +1,16 @@
+def add_reminder(reminders_list, text, date_str, category, username):
+    """Zadanie: Stwórz i dodaj nowe przypomnienie do listy."""
+    pass
+
+def delete_reminder(reminders_list, reminder_id):
+    """Zadanie: Usuń wybrane przypomnienie z listy."""
+    pass
+
+def edit_reminder(reminders_list, reminder_id, new_text=None, new_date=None, new_category=None):
+    """
+    Zadanie:
+    1. Znajdź w liście przypomnienie o konkretnym ID.
+    2. Jeśli użytkownik podał nową treść (new_text nie jest None), zaktualizuj pole w obiekcie.
+    3. To samo zrób dla daty i kategorii.
+    """
+    pass
