a425cb220164c0baeca2c61c41ffbe9b5cc4d83b
oxygenabuser <maciejoderenio@protonmail.com>
2025-12-23 12:27:06 +0100

    Zapisywanie oraz wczytywanie użytkowników
    Zapisywanie przyjmuje JEDEN obiekt typu User.
    Wczytywanie zwraca listę obiektów typu User.

--- storage.py
+++ storage.py
@@ -3,7 +3,7 @@ import os
 from models import *
 
 REMINDERS_PATH = "reminders.jsonl"
-
+USERS_PATH = "users.jsonl"
 
 def load_reminders():
     if not os.path.exists(REMINDERS_PATH) or os.path.getsize(REMINDERS_PATH) == 0:
@@ -23,6 +23,31 @@ def load_reminders():
             )
             reminders_list.append(new_reminder)
     return reminders_list
+
+def load_users():
+    if not os.path.exists(USERS_PATH) or os.path.getsize(USERS_PATH) == 0:
+        return []
+    users_list = []
+    with open(USERS_PATH, "r", encoding="utf-8") as file:
+        for line in file:
+            if not line.strip():
+                continue
+            item = json.loads(line)
+            user = User(
+                username=item.get("username"),
+                password_hash=item.get("password_hash")
+            )
+            users_list.append(user)
+    return users_list
+
+def save_user(new_user):
+    with open(USERS_PATH, "a", encoding="utf-8") as file:
+        data = {
+            "username": new_user.username,
+            "password_hash": new_user.password_hash,
+        }
+        file.write(json.dumps(data, ensure_ascii=False) + "\n")
+
 
 def save_reminder(new_reminder):
     with open(REMINDERS_PATH, "a", encoding="utf-8") as file:
