5f902b394aeaf86a3bc21f5c421e045494516f96
oxygenabuser <maciejoderenio@protonmail.com>
2025-12-23 11:37:23 +0100

    Wczytywanie i zapisywanie przypomnień oraz użytkowników w bazie
    Zapisane przypomnienie jest klasą typu Reminder. Zapisywanie przyjmuje JEDEN obiekt typu Reminder lub User.
    Wczytywanie przypomnień zwraca listę obiektów typu Remainder lub User.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import models
-import storage
+import datetime
+from models import *
+from storage import *
 import user
 import logic
 import filters
@@ -7,9 +8,8 @@ import interface
 
 def main():
     # Wczytanie danych z bazy
-    all_reminders = storage.load_data()
+    #all_reminders = storage.load_data()
     current_user = None
-
     while True:
         if not current_user:
             interface.show_main_menu()
--- storage.py
+++ storage.py
@@ -1,12 +1,60 @@
 import json
 import os
+from models import *
 
-FILE_PATH = "data.json"
+REMINDERS_PATH = "reminders.jsonl"
+USERS_PATH = "users.jsonl"
 
-def save_data(reminders_list):
-    """Zadanie: Zamień listę obiektów na JSON i zapisz do pliku."""
-    pass
+def load_reminders():
+    if not os.path.exists(REMINDERS_PATH) or os.path.getsize(REMINDERS_PATH) == 0:
+        return []
+    reminders_list = []
+    with open(REMINDERS_PATH, "r", encoding="utf-8") as file:
+        for line in file:
+            if not line.strip():
+                continue
+            item = json.loads(line)
+            new_reminder = Reminder(
+                id=item.get("id"),
+                user=item.get("user"),
+                text=item.get("text"),
+                category=item.get("category"),
+                due_date=item.get("due_date")
+            )
+            reminders_list.append(new_reminder)
+    return reminders_list
 
-def load_data():
-    """Zadanie: Wczytaj dane i zamień je na obiekty klasy Reminder."""
-    return []
+def load_users():
+    if not os.path.exists(USERS_PATH) or os.path.getsize(USERS_PATH) == 0:
+        return []
+    users_list = []
+    with open(USERS_PATH, "r", encoding="utf-8") as file:
+        for line in file:
+            if not line.strip():
+                continue
+            item = json.loads(line)
+            new_user = User(
+                username=item.get("username"),
+                password_hash=item.get("password_hash")
+            )
+            users_list.append(new_user)
+    return users_list
+
+def save_user(new_user):
+    with open(USERS_PATH, "a", encoding="utf-8") as file:
+        data = {
+            "username": new_user.username,
+            "password_hash": new_user.password_hash,
+        }
+        file.write(json.dumps(data, ensure_ascii=False) + "\n")
+
+def save_reminder(new_reminder):
+    with open(REMINDERS_PATH, "a", encoding="utf-8") as file:
+        data = {
+            "id": new_reminder.id,
+            "user": new_reminder.user,
+            "text": new_reminder.text,
+            "category": new_reminder.category,
+            "due_date": new_reminder.due_date.isoformat()
+        }
+        file.write(json.dumps(data, ensure_ascii=False) + "\n")
