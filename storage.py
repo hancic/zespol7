cc11421fc95f4c44881ea15ed165a83c22ac991d
oxygenabuser <maciejoderenio@protonmail.com>
2025-12-23 12:22:50 +0100

    Zapisywanie oraz wczytywanie przypomnień
    Zapisane przypomnienie jest klasą typu Reminder.
    Zapisywanie przyjmuje JEDEN obiekt typu Reminder.
    Wczytanie przypomnień zwraca listę obiektów typu Reminder.

--- storage.py
+++ storage.py
@@ -1,12 +1,36 @@
 import json
 import os
+from models import *
 
-FILE_PATH = "data.json"
+REMINDERS_PATH = "reminders.jsonl"
 
-def save_data(reminders_list):
-    """Zadanie: Zamień listę obiektów na JSON i zapisz do pliku."""
-    pass
 
-def load_data():
-    """Zadanie: Wczytaj dane i zamień je na obiekty klasy Reminder."""
-    return []
+def load_reminders():
+    if not os.path.exists(REMINDERS_PATH) or os.path.getsize(REMINDERS_PATH) == 0:
+        return []
+    reminders_list = []
+    with open(REMINDERS_PATH, "r", encoding="utf-8") as file:
+        for line in file:
+            if not line.strip():
+                continue
+            item = json.loads(line)
+            new_reminder = Reminder(
+                id=item.get("id"),
+                user=item.get("user"),
+                text=item.get("text"),
+                category=item.get("category"),
+                due_date=item.get("due_date")
+            )
+            reminders_list.append(new_reminder)
+    return reminders_list
+
+def save_reminder(new_reminder):
+    with open(REMINDERS_PATH, "a", encoding="utf-8") as file:
+        data = {
+            "id": new_reminder.id,
+            "user": new_reminder.user,
+            "text": new_reminder.text,
+            "category": new_reminder.category,
+            "due_date": new_reminder.due_date.isoformat()
+        }
+        file.write(json.dumps(data, ensure_ascii=False) + "\n")
