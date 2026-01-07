d8b181ad2138fa941c8ad3b018ca04f1660ea32c
pawi24 <359529@uwr.edu.pl>
2026-01-07 22:13:38 +0000

    obsługa main menu z błędem

--- interface.py
+++ interface.py
@@ -1,3 +1,6 @@
+import user
+import sys
+
 def show_main_menu():
     print("\n--- SYSTEM PRZYPOMNIEŃ ---")
     print("1. Zaloguj się")
@@ -14,3 +17,28 @@ def show_user_menu(username):
 
 def get_input(prompt):
     return input(prompt)
+
+def main_menu_action(all_reminders, all_users):
+	liczba = int(get_input("-->"))
+	if liczba == 1:
+		username = get_input("Nazwa użytkownika\n-->")
+		password = get_input("Hasło\n-->")
+		err = login_user(username, password, all_users)
+		if err:
+			print("Nie ma takiego użytkownika lub hasło jest błędne")
+			return None
+		else:
+			return username
+	elif liczba == 2:
+		username = get_input("Nazwa nowego użytkownika\n-->")
+		password1 = get_input("Hasło\n-->")
+		password2 = get_input("Potwierdź Hasło\n-->")
+		if password1 != password2:
+			print("Wprowadzone hasła różnią się\n")
+		else:
+			register_user(username, password, all_users)
+	elif liczba == 3:
+		sys.exit(0)
+	else:
+		print("Wprowadź 1, 2 lub 3")
+	return None
--- main.py
+++ main.py
@@ -6,19 +6,20 @@ import filters
 import interface
 
 def main():
-    # Wczytanie danych z bazy
-    all_reminders = storage.load_data()
-    current_user = None
+	# Wczytanie danych z bazy
+	all_reminders = storage.load_reminders()
+	all_users = storage.load_users()
+	current_user = None
 
-    while True:
-        if not current_user:
-            interface.show_main_menu()
-            # Tu dodasz logikę logowania/rejestracji
-            break # Tymczasowe zatrzymanie pętli
-        else:
-            interface.show_user_menu(current_user)
-            # Tu dodasz obsługę komend użytkownika
-            break
+	while True:
+		if not current_user:
+			interface.show_main_menu()
+			# Tu dodasz logikę logowania/rejestracji
+			current_user = interface.main_menu_action(all_reminders, all_users)
+		else:
+			interface.show_user_menu(current_user)
+			# Tu dodasz obsługę komend użytkownika
+			break
 
 if __name__ == "__main__":
-    main()
+	main()
